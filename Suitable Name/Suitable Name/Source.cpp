14e2623421d8726c61460dc3f0407e3b43292d29
Kevin Cozart <Cozartkevin@gmail.com>
2022-11-05 21:09:48 -0700

    Found Triangle Issue
    The issue was a - and not a = in the line: FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); of the Fragment Shader const at the top of the document.

--- Suitable Name/Suitable Name/Source.cpp
+++ Suitable Name/Suitable Name/Source.cpp
@@ -3,7 +3,7 @@
 #include <GLFW/glfw3.h>
 
 // Const Vertex Shader definition
-const char* vertexShaderSource = "#version 330 core \n"
+const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
@@ -15,7 +15,7 @@ const char* fragmentShaderSource = "#version 330 core \n"
 "out vec4 FragColor; \n"
 "void main()\n"
 "{\n"
-"	FragColor - vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
 
 //Function Prototypes
